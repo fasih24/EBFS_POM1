028f082e58ce75e2d4bd4e962e19d80db42edb27
Compilia Academy <info@compiliaonline.com>
2020-07-11 23:08:51 -0400

    initial Project Setup

---
+++ src/main/java/com/ebfs/qa/base/TestBase.java
@@ -0,0 +1,78 @@
+package com.ebfs.qa.base;
+
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import com.ebfs.qa.util.TestUtil;
+import com.ebfs.qa.util.WebEventListener;
+import com.ebfs.qa.properties.TestBaseProperties;
+
+
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+	public  static EventFiringWebDriver e_driver;
+	public static WebEventListener eventListener;
+	
+	public TestBase(){
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(TestBaseProperties.CONFIG_PATH);
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void initialization(){
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName.equals("chrome")){
+			System.setProperty(TestBaseProperties.CHROME_DRIVER, TestBaseProperties.CHROME_DRIVER_PATH);	
+			driver = new ChromeDriver(); 
+		}
+		else if(browserName.equals("FF")){
+			System.setProperty(TestBaseProperties.FIREFOX_DRIVER, TestBaseProperties.FIREFOX_DRIVER_PATH);	
+			driver = new FirefoxDriver(); 
+		}
+		
+		
+		e_driver = new EventFiringWebDriver(driver);
+		// Now create object of EventListerHandler to register it with EventFiringWebDriver
+		eventListener = new WebEventListener();
+		e_driver.register(eventListener);
+		driver = e_driver;
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/ebfs/qa/pages/TasksPage.java
@@ -0,0 +1,7 @@
+package com.ebfs.qa.pages;
+
+import com.ebfs.qa.base.TestBase;
+
+public class TasksPage extends TestBase{
+
+}
